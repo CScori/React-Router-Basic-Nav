a629b85ea47b473474e635f28f5a019ec103f79d
CScori <amilanotte1366@gmail.com>
2019-09-16 14:32:23 -0500

    adds route on app

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ const App = () => (
   </div>
 
   <Route exact path="/" component={Home}/>
+  <Route path="/about" component={About}/>
+  <Route path="/contact" component={Contact}/>
 );
 
 export default App;
